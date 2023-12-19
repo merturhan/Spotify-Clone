4e2b3397b427c515fc1f1c31b2e38ca34f773879
merturhan <mert-urhan@hotmail.com>
2023-12-19 16:08:07 +0300

    Initial Commit

---
+++ Spotify-Clone/Spotify_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_CloneApp.swift
+//  Spotify-Clone
+//
+//  Created by Mert Urhan on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
